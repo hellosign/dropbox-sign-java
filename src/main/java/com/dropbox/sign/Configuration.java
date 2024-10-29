3be6c9c795248a58147612357f70807e33092c7f
Juan Treminio <juantreminio@dropbox.com>
2024-10-29 15:02:07 -0500

    Release 1.7.0

--- src/main/java/com/dropbox/sign/Configuration.java
+++ src/main/java/com/dropbox/sign/Configuration.java
@@ -16,7 +16,7 @@ package com.dropbox.sign;
         value = "org.openapitools.codegen.languages.JavaClientCodegen",
         comments = "Generator version: 7.8.0")
 public class Configuration {
-    public static final String VERSION = "1.6.1";
+    public static final String VERSION = "1.7.0";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
